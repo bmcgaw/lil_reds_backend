440e10a3262ed2bfcf7215f40b974da76aaaabff
Brian McGaw <bmcgaw91@gmail.com>
2023-05-28 14:09:20 -0500

    second commit

--- server.js
+++ server.js
@@ -2,17 +2,20 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.use(express.static(path.join(__dirname + "/public")));
+app.listen(PORT, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "designsbylilred@gmail.com",
+    user: "designsbylilredd@gmail.com",
     pass: "dndlkkytjpnvccrb",
   },
 });
@@ -31,7 +34,7 @@ router.post("/contact", (req, res) => {
   const message = req.body.message;
   const mail = {
     from: name,
-    to: "bmcgaw91@gmail.com",
+    to: "designsbylilredd@gmail.com",
     subject: "Contact Form Submission",
     text: "Plaintext version of the message",
     html: `<p>Name: ${name}</p>
